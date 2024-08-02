1c9333aded09e1055056a7cb9860f1f8ef63a24b
Victor Rincon <vrinconaquino@gmail.com>
2024-08-02 19:28:48 -0400

    Feature (Post): Replace id for slug route key.

--- database/migrations/2024_08_02_211432_create_likes_table.php
+++ database/migrations/2024_08_02_211432_create_likes_table.php
@@ -10,7 +10,7 @@ return new class extends Migration {
         Schema::create('likes', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->foreignUlid('user_id');
-            $table->morphs('likeable');
+            $table->ulidMorphs('likeable');
             $table->timestamps();
         });
     }
